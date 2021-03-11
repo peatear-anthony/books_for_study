84b9b75b6285b8e545fa7df400eebc621a525349
Peter Sunga <peter.sunga@jp.ey.com>
2021-03-12 07:23:38 +0900

    use env for prof settigns DEBUG, ALLOWED_HOST, SECRET_KEY

--- tdd_with_python/superlists/settings.py
+++ tdd_with_python/superlists/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,14 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'am&pw9rt55r-fcp*$$8)(l4b-g9l!l@yw=d_c%h^au-&ya@la^'
+load_dotenv(verbose=True)
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
-
+if 'DJANGO_DEBUG_FALSE' in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
+else:
+    DEBUG = True
+    SECRET_KEY = 'insecure-key'
+    ALLOWED_HOSTS = []
 
 # Application definition
 
