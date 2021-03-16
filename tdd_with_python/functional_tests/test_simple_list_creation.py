d03ee0b44cefd9225868d93e1a8ea45725595322
Peter Sunga <peter.sunga@jp.ey.com>
2021-03-16 09:35:51 +0900

    moved tests into their own individual files

--- tdd_with_python/functional_tests/test_simple_list_creation.py
+++ tdd_with_python/functional_tests/test_simple_list_creation.py
@@ -1,44 +1,12 @@
 import os
-from dotenv import load_dotenv
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+import time
+
+from .base import FunctionalTest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import WebDriverException
-
-import time
-import unittest
-
-MAX_WAIT = 10
-load_dotenv(verbose=True)
-
-class NewVistorTest(StaticLiveServerTestCase):
-    def setUp(self):
-        self.chrome_options = Options()
-        self.chrome_options.add_argument('--headless') #  Hide browser
-        self.chrome_options.add_argument('--log-level=3')
-        self.browser = webdriver.Chrome(options=self.chrome_options)
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'http://' + staging_server
-
-    def tearDown(self):
-        self.browser.quit()
-
-    def wait_for_row_in_list_table(self, row_text):
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
 
 
+class NewVistorTest(FunctionalTest):
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
@@ -133,16 +101,3 @@ class NewVistorTest(StaticLiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, they both go back to sleep
-
-    def test_layout_and_styling(self):
-        # Edith goes to the home page
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
-
-        # She notices the input box is nicely centered
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=10
-        )
