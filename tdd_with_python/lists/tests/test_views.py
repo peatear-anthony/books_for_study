5a916800d7b983e72b30031cc796cf8203f8b8ac
Peter Sunga <peter.sunga@jp.ey.com>
2021-03-18 13:39:30 +0900

    enforce model validation in list view

--- tdd_with_python/lists/tests/test_views.py
+++ tdd_with_python/lists/tests/test_views.py
@@ -49,7 +49,6 @@ class ListViewTest(TestCase):
         response = self.client.get(f'/lists/{list_.id}/')
         self.assertTemplateUsed(response, 'list.html')
 
-
     def test_displays_all_list_items(self):
         correct_list = List.objects.create()
         Item.objects.create(text='item 1', list=correct_list)
@@ -95,3 +94,14 @@ class ListViewTest(TestCase):
         )
 
         self.assertRedirects(response, f'/lists/{correct_list.id}/')
+
+    def test_validation_errors_end_up_on_lists_page(self):
+        list_ = List.objects.create()
+        response = self.client.post(
+            f'/lists/{list_.id}/',
+            data={'item_text': ''}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'list.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
--- tdd_with_python/lists/views.py
+++ tdd_with_python/lists/views.py
@@ -14,10 +14,18 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = models.List.objects.get(id=list_id)
+    error = None
     if request.method == 'POST':
-        models.Item.objects.create(text=request.POST['item_text'], list=list_)
-        return redirect(f'/lists/{list_.id}/')
-    return render(request, 'list.html', {'list': list_})
+        try:
+            item = models.Item.objects.create(text=request.POST['item_text'],
+                                      list=list_)
+            item.full_clean()
+            item.save()
+            return redirect(f'/lists/{list_.id}/')
+        except ValidationError:
+            item.delete()
+            error = "You can't have an empty list item"
+    return render(request, 'list.html', {'list': list_, 'error': error})
 
 def new_list(request):
     list_ = models.List.objects.create()
